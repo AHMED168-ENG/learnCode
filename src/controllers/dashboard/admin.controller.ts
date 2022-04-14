d4932eb3f1d0feecf308dd84a2e119813a5a5327
DESKTOP-C3P8RS6\eramo <eramonetsolutions@gmail.com>
2022-04-14 13:44:08 +0200

    fix issues in admin component

--- src/controllers/dashboard/admin.controller.ts
+++ src/controllers/dashboard/admin.controller.ts
@@ -3,7 +3,7 @@ import httpStatus from "http-status"
 import admin from "../../models/admin.model"
 import helpers from "../../helper/helpers";
 import { IAdminUser } from "../../interfaces/IAdminUser";
-
+import bcrypt from "bcrypt";
 export class AdminController {
   loginPage(req: Request, res: Response, next: NextFunction) {
     return res.render("login.ejs", { title: "Login" });
@@ -64,15 +64,20 @@ export class AdminController {
       if (!helpers.regularExprEmail(createdAdminData.email)) return res.status(httpStatus.BAD_REQUEST).json({ message: "invalid email" });
       const existedAdmin = await admin.findOne({ where: { email: createdAdminData.email } });
       if (existedAdmin) return res.status(httpStatus.NOT_ACCEPTABLE).json({ message: "This email is already exists" });
-      await admin.create(req.body);
+      const password = await bcrypt.hash(createdAdminData.password, 10);
+      const existedAdminWithPass = await admin.findOne({ where: { password } });
+      if (existedAdminWithPass) return res.status(httpStatus.NOT_ACCEPTABLE).json({ message: "This password is already exists" });
+      const request = { fullName: createdAdminData.fullName, email: createdAdminData.email, phone: createdAdminData.phone, role_id: createdAdminData.role_id, password };
+      await admin.create(request);
       return res.status(httpStatus.OK).json({ msg: "new admin created" });
     } catch (error) {
+      console.log(error)
       return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ error, messgae: "Can't add admin" });
     }
   }
   async editPage(req: Request, res: Response, next: NextFunction) {
     try {
-      const data = await admin.findOne({ where: { id: req.params.id }, attributes: { exclude: ["id"] }, raw: true });
+      const data = await admin.findOne({ where: { id: req.params.id }, attributes: { exclude: ["id", "createdAt", "updatedAt"] }, raw: true });
       return res.render("admin/edit.ejs", { title: "Edit admin", data });
     } catch (error) {
       return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ error, message: "Can't get admin data for edit page" });
@@ -80,9 +85,13 @@ export class AdminController {
   }
   async edit(req: Request, res: Response, next: NextFunction) {
     try {
-      const createdAdminData: IAdminUser = req.body;
-      if (!createdAdminData || ![1, 2].includes(Number(createdAdminData.role_id))) return res.status(httpStatus.BAD_REQUEST).json({ message: "Bad Request" });
-      if (!helpers.regularExprEmail(createdAdminData.email)) return res.status(httpStatus.BAD_REQUEST).json({ message: "invalid email" });
+      const updatedAdminData: IAdminUser = req.body;
+      if (!updatedAdminData || ![1, 2].includes(Number(updatedAdminData.role_id))) return res.status(httpStatus.BAD_REQUEST).json({ message: "Bad Request" });
+      if (updatedAdminData.email && !helpers.regularExprEmail(updatedAdminData.email)) return res.status(httpStatus.BAD_REQUEST).json({ message: "invalid email" });
+      if (updatedAdminData.password) {
+        const password: string = await bcrypt.hash(updatedAdminData.password, 5);
+        req.body.password = password;
+      }
       await admin.update(req.body, { where: { id: req.params.id } });
       return res.status(httpStatus.OK).json({ msg: "admin edited" });
     } catch (error) {

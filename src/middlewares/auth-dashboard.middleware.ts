ee5a6b4ea228bdb8a0a9aef13b463b9a62813175
DESKTOP-C3P8RS6\eramo <eramonetsolutions@gmail.com>
2022-07-17 11:14:56 +0200

    fix unauthorize issue in auth middleware

--- src/middlewares/auth-dashboard.middleware.ts
+++ src/middlewares/auth-dashboard.middleware.ts
@@ -2,7 +2,7 @@ import httpStatus from "http-status"
 
 export = (req, res, next) => {
   const excluded = ["login", "register"]
-  if (excluded.indexOf(req.url.split("/")[3]) > -1 || excluded.indexOf(req.url.split("/")[2]) > -1) {
+  if (excluded.indexOf(req.url.split("/")[3]) > -1 || excluded.indexOf(req.url.split("/")[2]) > -1 || req.url.split("/")[1] === "v1") {
     return next()
   } else {
     if (req.cookies.token) {
